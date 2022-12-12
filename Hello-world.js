1a3c9f0b84dd283d432e7108eb682082ff52b745
Aaron Chris <69767247+aaronteamAAR@users.noreply.github.com>
2022-12-12 11:43:32 +0100

    Hello-world exercise

---
+++ Hello-world.js
@@ -0,0 +1,14 @@
+//
+2
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+3
+// convenience to get you started writing code faster.
+4
+//
+5
+6
+export function hello() {
+7
+  return "Hello, World!";
+8
+}
