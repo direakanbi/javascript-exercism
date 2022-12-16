accd89056a7f84623d0c47fb13ae99033814b211
Dire Akanbi <direakanbi@icloud.com>
2022-12-16 08:57:01 -0800

    Enchantment solution

--- Hello-world.js
+++ Hello-world.js
@@ -2,10 +2,12 @@
 // This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 
+let array = ['foo', 'bar', 10, 'qux'];
 
-
-export function hello() {
-
-  return "Hello, World!";
-
+const setItem = (arr, positions, replacementCard) => {
+  return arr.splice(positions, 1, replacementCard);
 }
+
+
+console.log(setItem(array, 1, "foot"))
+console.log(array)
